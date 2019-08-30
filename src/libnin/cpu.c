3c751bcce145dc59ed14f61b35ff4713d90521a4
Nax <max@bacoux.com>
2019-08-30 02:48:51 +0200

    wip

--- src/libnin/cpu.c
+++ src/libnin/cpu.c
@@ -59,6 +59,52 @@ static uint16_t stackPop16(NinState* state)
     return lo | ((uint16_t)hi << 8);
 }
 
+static uint8_t asl(NinState* state, uint8_t v)
+{
+    state->cpu.p &= ~PFLAG_C;
+    if (v & 0x80) state->cpu.p |= PFLAG_C;
+
+    return (v << 1);
+}
+
+static uint8_t rol(NinState* state, uint8_t v)
+{
+    uint8_t carry;
+
+    carry = (state->cpu.p & PFLAG_C) ? 0x00 : 0x01;
+    state->cpu.p &= ~PFLAG_C;
+    if (v & 0x80) state->cpu.p |= PFLAG_C;
+
+    return ((v << 1) | carry);
+}
+
+static uint8_t lsr(NinState* state, uint8_t v)
+{
+    state->cpu.p &= ~PFLAG_C;
+    if (v & 0x01) state->cpu.p |= PFLAG_C;
+
+    return (v >> 1);
+}
+
+static uint8_t ror(NinState* state, uint8_t v)
+{
+    uint8_t carry;
+
+    carry = (state->cpu.p & PFLAG_C) ? 0x00 : 0x80;
+    state->cpu.p &= ~PFLAG_C;
+    if (v & 0x01) state->cpu.p |= PFLAG_C;
+
+    return ((v >> 1) | carry);
+}
+
+static uint8_t bit(NinState* state, uint8_t v)
+{
+    state->cpu.p &= (0xc0);
+    state->cpu.p |= (v & 0xc0);
+
+    return state->cpu.regs[REG_A] & v;
+}
+
 static uint8_t _adc(NinState* state, uint8_t a, uint8_t b)
 {
     uint16_t sum;
@@ -98,7 +144,7 @@ static const uint8_t kHex[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 
 static const uint8_t kBranchFlags[] = { PFLAG_N, PFLAG_V, PFLAG_C, PFLAG_Z };
 
 #define X(n, str, code) do { if ((kHex[str[(n) / 4] - '0']) & (1 << ((n) % 4))) code } while (0)
-#define CYCLE()         do { cyc++; ninPpuRunCycles(state, 3); } while (0)
+#define CYCLE()         do { cyc++; frame |= ninPpuRunCycles(state, 3); ninRunCyclesAPU(state, 1); } while (0)
 #define READ(x)         do { tmp = ninMemoryRead8(state, (x)); } while (0)
 
 #define INSTRUCTION(n)                                                                                                                                          \
@@ -118,12 +164,14 @@ do                                                                              
     default: break;                                                                                                                                 \
     }                                                                                                                                               \
                                                                                                                                                     \
-    X(n, "0000000000000000000000000000000000000000000000002222222200000000", { r0 = REG_A; });                                                      \
-    X(n, "0000000000000000000000000000000000000000000000000040000011110000", { r0 = REG_X; });                                                      \
+    X(n, "0040000000400000005000000040000022222222005000002222222200000000", { r0 = REG_A; });                                                      \
+    X(n, "0000000000000000000000000000000006660660000000000040000011110000", { r0 = REG_X; });                                                      \
     X(n, "0000000000000000000000000000000000000000000000001111100000000000", { r0 = REG_Y; });                                                      \
-    X(n, "0000000000000000000000000000000000000000222222220000000000000000", { r1 = REG_A; });                                                      \
-    X(n, "0000000000000000000000000000000000000000444404440040000000100000", { r1 = REG_X; });                                                      \
+    X(n, "0000000000000000000000000000000000000000000000600000000000000000", { r0 = REG_S; });                                                      \
+    X(n, "2262222222622222226222222272222200000010222222220000000022222222", { r1 = REG_A; });                                                      \
+    X(n, "0000000000000000000000000000000000000000444404640040000000100000", { r1 = REG_X; });                                                      \
     X(n, "0000000000000000000000000000000000000000111101010010000000000000", { r1 = REG_Y; });                                                              \
+    X(n, "0000000000000000000000000000000000000060000000000000000000000000", { r1 = REG_S; });                                                              \
     X(n, "2000060620000606200006062000060620000302200003032000060620000606", { i = state->cpu.regs[REG_X]; });                          /* index-X */       \
     X(n, "0000002000000020000000200000002000000420000004240000002000000020", { i = state->cpu.regs[REG_Y]; });                          /* index-Y */       \
     X(n, "2626362637273626262736262627362627073722772737273727362637273626", { tmp = ninMemoryRead8(state, pc++); addr = tmp; });       /* op1 */           \
@@ -133,19 +181,28 @@ do                                                                              
     X(n, "2000200020002000200020002001200020002000200020002000200020002000", { CYCLE(); addr = indirect(state, addr); });               /* indirect */      \
     X(n, "0000200000002000000020000000200000002000000020000000200000002000", { CYCLE(); addr += state->cpu.regs[REG_Y]; });             /* post-index */    \
     X(n, "2606262627072626260626262606262600000000270727272707262627072626", { CYCLE(); READ(addr); });                                 /* mem */           \
-    X(n, "0000000000000000000000000000000000000000000000000000000000000000", { tmp = state->cpu.regs[r0]; });                           /* reg -> tmp */    \
+    X(n, "0040000000400000005000000040000027772772005000400050000000100000", { tmp = state->cpu.regs[r0]; });                           /* reg -> tmp */    \
+    X(n, "0010000000000000000000000000000000000000000000000000000000000000", { tmp = (state->cpu.p | ~PFLAG_MASK); });                  /* flags -> tmp */  \
     X(n, "2222222200000000000000000000000000000000000000000000000000000000", { tmp = state->cpu.regs[REG_A] | tmp; });                  /* ora */           \
     X(n, "0000000022222222000000000000000000000000000000000000000000000000", { tmp = state->cpu.regs[REG_A] & tmp; });                  /* and */           \
     X(n, "0000000000000000222222220000000000000000000000000000000000000000", { tmp = state->cpu.regs[REG_A] ^ tmp; });                  /* eor */           \
     X(n, "0000000000000000000000002222222200000000000000000000000000000000", { tmp = _adc(state, state->cpu.regs[REG_A], tmp); });      /* adc */           \
     X(n, "0000000000000000000000000000000000000000000000000010000004140404", { tmp++; });                                               /* inc */           \
     X(n, "0000000000000000000000000000000000100000000000000444040400000000", { tmp--; });                                               /* dec */           \
-    X(n, "0000000000000000000000000000000027072722000000000404040404040404", { CYCLE(); ninMemoryWrite8(state, addr, tmp); });          /* store */         \
-    X(n, "0000000000000000000000000000000000100000777727670000000000000000", { state->cpu.regs[r1] = tmp; });                           /* load */          \
+    X(n, "0444040400000000000000000000000000000000000000000000000000000000", { tmp = asl(state, tmp); });                               /* asl */           \
+    X(n, "0000000004440404000000000000000000000000000000000000000000000000", { tmp = rol(state, tmp); });                               /* rol */           \
+    X(n, "0000000000000000044404040000000000000000000000000000000000000000", { tmp = lsr(state, tmp); });                               /* lsr */           \
+    X(n, "0000000000000000000000000444040400000000000000000000000000000000", { tmp = ror(state, tmp); });                               /* ror */           \
+    X(n, "0000000001010000000000000000000000000000000000000000000000000000", { tmp = bit(state, tmp); });                               /* bit */           \
+    X(n, "0010000000000000001000000000000000000000000000000000000000000000", { stackPush8(state, tmp); });                              /* push */          \
+    X(n, "0000000000100000000000000010000000000000000000000000000000000000", { tmp = stackPop8(state); });                              /* pop */           \
+    X(n, "0404040404040404040404040404040427072722000000000404040404040404", { CYCLE(); ninMemoryWrite8(state, addr, tmp); });          /* store */         \
+    X(n, "2262222222622222226222222272222201110100777727670040001022322222", { state->cpu.regs[r1] = tmp; });                           /* load */          \
+    X(n, "0000000000100000000000000000000000000000000000000000000000000000", { state->cpu.p = (tmp & PFLAG_MASK); });                   /* tmp -> flags */  \
     X(n, "0000000000000000000000000000000000000000000000000000000022222222", { tmp = _adc(state, state->cpu.regs[REG_A], ~tmp); });     /* sbc */           \
     X(n, "0000000000000000000000000000000000000000000000003323222211010000", { tmp = compare(state, state->cpu.regs[r0], tmp); });      /* cmp */           \
-    X(n, "2666266627672666266626662666005000107777276737777777262637372626", { flagZ(state, tmp); });                                   /* Z */             \
-    X(n, "2666266627672666266626662666005000107777276737777777262637372626", { flagN(state, tmp); });                                   /* N */             \
+    X(n, "2666266727672666266626662676042600107777777737777777262637372626", { flagZ(state, tmp); });                                   /* Z */             \
+    X(n, "2666266726662666266626662676042600107777777737777777262637372626", { flagN(state, tmp); });                                   /* N */             \
     X(n, "0000000000000000100000000000000000000000000000000000000000000000", { state->cpu.p = (stackPop8(state) & PFLAG_MASK); pc = stackPop16(state); }); /* rti */    \
     X(n, "0000000000000000000000001000000000000000000000000000000000000000", { pc = stackPop16(state) + 1; }); /* rts */        \
     X(n, "0000000010000000000000000000000000000000000000000000000000000000", { stackPush16(state, pc - 1); }); /* jsr */        \
@@ -170,7 +227,9 @@ void ninRunFrameCPU(NinState* state)
     uint8_t r0;
     uint8_t r1;
     uint8_t i;
-    uint16_t cyc;
+    uint64_t cyc;
+
+    int frame;
 
     pc = state->cpu.pc;
     cyc = 0;
@@ -185,18 +244,19 @@ void ninRunFrameCPU(NinState* state)
             stackPush8(state, state->cpu.p);
             printf("--- NMI ---\n");
             fflush(stdout);
-            getchar();
+            //getchar();
             pc = ninMemoryRead16(state, 0xfffa);
             state->cpu.pc = pc;
         }
 
         cyc += 2;
-        printf("CYC:%05d PC:0x%04x ", cyc, pc);
-        cyc++;
+        printf("CYC:%010lld PC:0x%04x ", cyc, pc);
         op = ninMemoryRead8(state, pc++);
         printf("OP:0x%02x A:0x%02x X:0x%02x Y:0x%02x S:0x%02x P:0x%02x\n", op, state->cpu.regs[REG_A], state->cpu.regs[REG_X], state->cpu.regs[REG_Y], state->cpu.regs[REG_S], state->cpu.p);
         fflush(stdout);
         //getchar();
+        ninRunCyclesAPU(state, 2);
+        frame = ninPpuRunCycles(state, 6);
 
         switch (op)
         {
@@ -206,8 +266,7 @@ void ninRunFrameCPU(NinState* state)
             ICASE4(0xc0);
         }
 
-        if (ninPpuRunCycles(state, 3))
-            break;
+        if (frame) break;
     }
 
     state->cpu.pc = pc;
