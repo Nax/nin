8f8f2d4d4732fae4e826698f2c51440486a26e28
Nax <max@bacoux.com>
2019-12-22 23:31:35 +0100

    Fix some warnings

--- src/libnin/mapper/mmc4.c
+++ src/libnin/mapper/mmc4.c
@@ -30,7 +30,6 @@
 
 void ninPrgWriteHandlerMMC4(NinState* state, uint16_t addr, uint8_t value)
 {
-    NinMapperRegsMMC2* mmc2 = &state->mapperRegs.mmc2;
     ninPrgWriteHandlerCommonMMC2_MMC4(state, addr, value);
     switch (addr & 0xf000)
     {
--- src/ninconv/ninconv.c
+++ src/ninconv/ninconv.c
@@ -26,6 +26,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <locale.h>
