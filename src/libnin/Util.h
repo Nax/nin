50cf67d90be649e08fa784aa23170531d8471180
Nax <max@bacoux.com>
2020-02-09 19:38:44 +0100

    Removed useless zalloc helper

--- src/libnin/Util.h
+++ src/libnin/Util.h
@@ -1,14 +1,8 @@
 #ifndef LIBNIN_UTIL_H
 #define LIBNIN_UTIL_H 1
 
-#define UNUSED(x)   ((void)x)
+#include <cstddef>
 
-template <typename T>
-inline static T* zalloc(size_t count)
-{
-    if (count)
-        return new T[count]();
-    return nullptr;
-}
+#define UNUSED(x)   ((void)x)
 
 #endif
