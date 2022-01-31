00eac2d6fb77076ab9ad4a4656b6dd731cf4add2
Maxime Bacoux <max@bacoux.com>
2022-01-31 17:49:58 +0100

    Update the copyright headers

--- src/NinEmu/CMakeLists.txt
+++ src/NinEmu/CMakeLists.txt
@@ -1,23 +1,23 @@
 # Nin, a Nintendo Entertainment System Emulator.
-#
-# Copyright (c) 2018-2020 Maxime Bacoux
+# 
+# Copyright (c) 2018-2022 Maxime Bacoux
 # All rights reserved.
-#
+# 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # Version 2, as published by the Free Software Foundation.
-#
+# 
 # Alternatively, this program can be licensed under a commercial license
 # upon request.
-#
+# 
 # When using the program under the GNU General Public License Version 2 license,
 # the following apply:
-#
+# 
 #  1. This program is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-#
+# 
 #  2. You should have received a copy of the GNU General Public License
 #     along with this program; if not, write to the Free Software
 #     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- src/ninconv/dirent.h
+++ src/ninconv/dirent.h
@@ -1,11 +1,29 @@
 /*
- * Dirent interface for Microsoft Visual Studio
- *
- * Copyright (C) 1998-2019 Toni Ronkko
- * This file is part of dirent.  Dirent may be freely distributed
- * under the MIT license.  For all details and documentation, see
- * https://github.com/tronkko/dirent
+ * Nin, a Nintendo Entertainment System Emulator.
+ * 
+ * Copyright (c) 2018-2022 Maxime Bacoux
+ * All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * Version 2, as published by the Free Software Foundation.
+ * 
+ * Alternatively, this program can be licensed under a commercial license
+ * upon request.
+ * 
+ * When using the program under the GNU General Public License Version 2 license,
+ * the following apply:
+ * 
+ *  1. This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *  2. You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 #ifndef DIRENT_H
 #define DIRENT_H
 
